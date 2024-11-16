4fb62fdbf87c78670939f860a4540733d360e45a
Daniel <923daniel@naver.com>
2024-11-16 22:09:58 +0900

    refactor: refresh토큰 서버에서 쿠키에 담도록 수정

--- src/pages/LoginSuccess.js
+++ src/pages/LoginSuccess.js
@@ -7,17 +7,11 @@ function LoginSuccess() {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const access = urlParams.get('access');
-    const refresh = urlParams.get('refresh');
 
-    if (access && refresh) {
+    if (access) {
       // JWT 토큰을 로컬 스토리지에 저장
       localStorage.setItem('accessToken', access);
 
-      // 리프레시 토큰을 쿠키에 저장
-      document.cookie = `refreshToken=${refresh}; path=/; SameSite=None; Secure; Domain=.nolgoat.site`;
-
-
-      // 원하는 페이지로 리다이렉트
       navigate('/');
     } else {
       // 토큰이 없을 경우 에러 처리 (예: 로그인 페이지로 리다이렉트)
